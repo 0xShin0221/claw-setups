a450a7cca34aee86804bfeafc40d649f876676e4
Shin <sintaronettt@gmail.com>
2026-03-21 20:00:23 +0900

    fix: likes fallback to 0 when undefined

--- src/components/SetupCard.tsx
+++ src/components/SetupCard.tsx
@@ -21,7 +21,7 @@ export default function SetupCard({ setup }: { setup: Setup }) {
             <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
               <path d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
             </svg>
-            {setup.likes}
+            {setup.likes ?? 0}
           </div>
         </div>
 
